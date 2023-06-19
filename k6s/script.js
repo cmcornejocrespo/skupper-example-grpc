494b11616ff5bbfed250f182c3f099e32afad733
Carlos M. Cornejo <ccornejo@redhat.com>
2023-06-19 13:31:28 +0200

    added BDD js libs cosmetic chanhes

--- k6s/script.js
+++ k6s/script.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
 
-// const endpoint = 'http://frontend-external-west.apps.ocp-skupper-acm.r7b7x.azure.redhatworkshops.io';
 const endpoint = `${__ENV.FRONT}`;
 
 const products = [
@@ -44,13 +44,13 @@ export const options = {
             executor: 'constant-arrival-rate',
             exec: 'setCurrency',
             // How long the test lasts
-            duration: '5m30s',
+            duration: '30s',
             // How many iterations per timeUnit
-            rate: 50,
+            rate: 10,
             // Start `rate` iterations per second
             timeUnit: '1s',
             // Pre-allocate VUs
-            preAllocatedVUs: 100,
+            preAllocatedVUs: 50,
         },
         browseProduct: {
             executor: 'ramping-arrival-rate',
@@ -63,86 +63,100 @@ export const options = {
                 { duration: '1m', target: 50 },
             ],
             // Pre-allocate VUs
-            preAllocatedVUs: 500,
+            preAllocatedVUs: 250,
         },
         viewCart: {
             executor: 'ramping-arrival-rate',
             exec: 'viewCart',
             stages: [
-                { duration: '30s', target: 100 },
+                { duration: '30s', target: 50 },
                 // { duration: '1m30s', target: 500 },
                 // { duration: '1m30s', target: 5000 },
                 // { duration: '2m', target: 500 },
-                { duration: '1m', target: 100 },
+                { duration: '1m', target: 50 },
             ],
             // Pre-allocate VUs
-            preAllocatedVUs: 200,
+            preAllocatedVUs: 250,
         },
         addToCart: {
             executor: 'ramping-arrival-rate',
             exec: 'addToCart',
             stages: [
-                { duration: '30s', target: 100 },
+                { duration: '30s', target: 10 },
                 // { duration: '1m30s', target: 500 },
                 // { duration: '1m30s', target: 5000 },
                 // { duration: '2m', target: 500 },
-                { duration: '1m', target: 100 },
+                { duration: '1m', target: 10 },
             ],
             // Pre-allocate VUs
-            preAllocatedVUs: 200,
+            preAllocatedVUs: 50,
         },
         checkout: {
             executor: 'ramping-arrival-rate',
             exec: 'checkout',
             stages: [
                 { duration: '1m30s', target: 50 },
-                { duration: '30s', target: 100 },
+                // { duration: '30s', target: 100 },
                 // { duration: '1m30s', target: 200 },
-                { duration: '2m', target: 100 },
+                // { duration: '2m', target: 100 },
                 { duration: '1m', target: 50 },
             ],
             // Pre-allocate VUs
-            preAllocatedVUs: 400,
+            preAllocatedVUs: 250,
         },
     }
 };
 
 export function index() {
-    const res = http.get(endpoint);
-    check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+    // BDD style
+    describe('Loads the boutique frontend', () => {
+        // when
+        const response = http.get(endpoint);
+
+        //then
+        expect(response.status, 'response status').to.equal(200);
+        sleep(1 + Math.random()); // sleep between 1s and 2s
+    });
 }
+
 export function browseProduct() {
-    const res = http.get(endpoint + '/product/' + randomItem(products));
-    check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+
+    // BDD style
+    describe('Browse random products', () => {
+        //given
+        const product = randomItem(products);
+        // when
+        const response = http.get(endpoint + '/product/' + product);
+        //then
+        expect(response.status, 'response status').to.equal(200);
+        sleep(1 + Math.random()); // sleep between 1s and 2s
+    });
 }
 
 export function setCurrency() {
-    const payload = JSON.stringify({
+
+    const payload = {
         currency_code: randomItem(currencies),
-      });
-    const res = http.post(endpoint + '/setCurrency', payload, {
-        headers: { 'Content-Type': 'application/json' }
-    });
+    };
+
+    const res = http.post(endpoint + '/setCurrency', payload, {});
+
     check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+    sleep(1 + Math.random()); // sleep between 1s and 2s
 }
 
 export function viewCart() {
+
     const res = http.get(endpoint + '/cart');
+
     check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+
+    sleep(1 + Math.random()); // sleep between 1s and 2s
 }
 
 export function addToCart() {
     const product = randomItem(products);
     http.get(endpoint + '/product/' + product);
-
-    // const payload = JSON.stringify({
-    //     product_id: product,
-    //     quantity: randomItem([1,2,3,4,5,10]),
-    //   });
 
     const payload = {
         product_id: product,
@@ -152,7 +166,7 @@ export function addToCart() {
     const resAddCart = http.post(endpoint + '/cart', payload, {});
 
     check(resAddCart, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+    sleep(1 + Math.random()); // sleep between 1s and 2s
 }
 
 export function checkout() {
@@ -174,7 +188,6 @@ export function checkout() {
 
     const res = http.post(endpoint + '/cart/checkout', payload, {});
 
-    check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
-
+    check(res, { 'status was 200': (r) => r.status == 200 , 'protocol is HTTP/2': (r) => r.proto === 'HTTP/2.0', });
+    sleep(1 + Math.random()); // sleep between 1s and 2s
 }
